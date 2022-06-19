59197384486bb8afbf834d2286b295a4413915fe
Dworv <dwarvyt@gmail.com>
2022-06-19 16:00:12 -0700

    feat: add init for main ext

--- interactions/ext/permapages/permapages.py
+++ interactions/ext/permapages/permapages.py
@@ -1,6 +1,16 @@
+from pathlib import Path
+from typing import Union
+
+from .fs import PagesFolder
+
 from interactions import Extension
 
 class PermaPages(Extension):
     """
     The main class for the PermaPages extension.
     """
+    def __init__(self, bot, path: Union[Path, str]=None):
+        self.bot = bot
+        self.folder = PagesFolder(path)
+        
+        print(self.folder.get_page("test", 1))
